1c1815d084f1cc1f2748d03b505046447fb90737
CheesyChocolate <dev@behnamlal.xyz>
2025-12-29 00:53:38 +0000

    feat: add favicon

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 set -e
 
-# Load .env file
 if [ -f .env ]; then
-    # Export variables, ignoring lines starting with #
     export $(grep -v '^#' .env | xargs)
 else
     echo ".env file not found"
     exit 1
 fi
 
-# Ensure mandatory variables are set
 : "${REMOTE_USER:?REMOTE_USER is not set in .env}"
 : "${REMOTE_HOST:?REMOTE_HOST is not set in .env}"
 : "${REMOTE_PATH:?REMOTE_PATH is not set in .env}"
@@ -22,13 +19,7 @@ git pull
 echo "--- Pushing changes ---"
 git push
 
-# If you add a build step later (like zola or npm build), add it here
-# echo "--- Building ---"
-# zola build 
-
 echo "--- Deploying to ${REMOTE_HOST} ---"
-# Syncing files. 
-# We exclude git metadata and the deployment config itself.
 rsync -vrPlu \
     --exclude='.git/' \
     --exclude='.env' \
@@ -36,4 +27,3 @@ rsync -vrPlu \
     "${LOCAL_DIR}/" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}"
 
 echo "--- Deployment Complete ---"
-
